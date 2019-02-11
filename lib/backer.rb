b90089a9585fb25bd37d35ab6c18f5aab873f67f
John Hayes <johnnyhayes7@gmail.com>
2019-02-11 19:56:02 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -15,7 +15,8 @@ class Backer
   
   def back_project(project)
     @backed_projects << project
-    project.add_backer(self)
+    if !project.backers.include?(self)
+      project.add_backer(self)
     
   end
   
