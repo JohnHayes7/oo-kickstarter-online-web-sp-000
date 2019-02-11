796ca904a2866e21eec6f517ce3b6d8d0920894a
John Hayes <johnnyhayes7@gmail.com>
2019-02-11 19:53:38 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,20 @@
+require "pry"
+
+class Project
+  attr_reader :title
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def backers
+    @backers
+  end
+  
+  def add_backer(backer_name)
+    @backers << backer_name
+    
+  end
+  
+end
